4f1ea2f3dd67193aa57e1741566b7be106d3b8ab
Celine Hernandez <celine.hernandez@ens.fr>
2014-09-22 18:18:55 +0200

    Motifs used in the article.
    Commit of motifs used in the article.

---
+++ data/inputs/motif/MA0139.1.tf
@@ -0,0 +1,46 @@
+AC  MA0139.1
+XX
+ID  CTCF
+XX
+DE  MA0139.1 CTCF; from JASPAR
+PO       A     C     G     T
+1       87   291    76   459
+2      167   145   414   187
+3      281    49   449   134
+4       56   800    21    36
+5        8   903     0     2
+6      744    13    65    91
+7       40   528   334    11
+8      107   433    48   324
+9      851    11    32    18
+10       5     0   903     3
+11     333     3   566     9
+12      54    12   504   341
+13      12     0   890     8
+14      56     8   775    71
+15     104   733     5    67
+16     372    13   507    17
+17      82   482   307    37
+18     117   322    73   396
+19     402   181   266    59
+XX
+CC  program: jaspar
+CC  matrix.nb: 87
+CC  min.prior: 0.25
+CC  alphabet.size: 4
+CC  max.bits: 2
+CC  total.information: 11.8879375984146
+CC  information.per.column: 0.625680926232349
+CC  max.possible.info.per.col: 1.38629436111989
+CC  consensus.strict: tggCCAccAGggGGCgcta
+CC  consensus.strict.rc: TAGCGCCCCCTGGTGGCCA
+CC  consensus.IUPAC: ygrCCAsyAGrkGGCrsyr
+CC  consensus.IUPAC.rc: YRSYGCCMYCTRSTGGYCR
+CC  consensus.regexp: [ct]g[ag]CCA[cg][ct]AG[ag][gt]GGC[ag][cg][ct][ag]
+CC  consensus.regexp.rc: [CT][AG][CG][CT]GCC[AC][CT]CT[AG][CG]TGG[CT]C[AG]
+CC  residues.content.crude.freq: a:0.2240|c:0.2846|g:0.3603|t:0.1311
+CC  G+C.content.crude.freq: 0.644893722938517
+CC  residues.content.corrected.freq: a:0.2240|c:0.2845|g:0.3602|t:0.1312
+CC  G+C.content.corrected.freq: 0.644734786558118
+XX
+//
---
+++ data/inputs/motif/oligos_6nt_mkv4_m5.tf
@@ -0,0 +1,31 @@
+AC  oligos_6nt_mkv4_m5
+XX
+ID  oligos_6nt_mkv4_m5
+XX
+DE  rmACATTCCAkw
+PO       A     C     G     T
+1      336   186   416   296
+2      402   344   194   294
+3     1234     0     0     0
+4        0  1234     0     0
+5     1234     0     0     0
+6        0   121   177   936
+7      303     0     0   931
+8        0  1234     0     0
+9        0  1234     0     0
+10    1229     0     5     0
+11     290   166   415   363
+12     369   213   301   351
+XX
+CC  program: feature
+CC  matrix.nb: 5
+CC  matrix.nb: 5
+CC  sites: 1234
+CC  consensus.strict: gaACATTCCAga
+CC  consensus.strict.rc: TCTGGAATGTTC
+CC  consensus.IUPAC: rmACATTCCAkw
+CC  consensus.IUPAC.rc: WMTGGAATGTKY
+CC  consensus.regexp: [ag][ac]ACATTCCA[gt][at]
+CC  consensus.regexp.rc: [AT][AC]TGGAATGT[GT][CT]
+XX
+//
